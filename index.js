859a0a515ea49d939d4ae3007d9d1614ba6fc632
AwsomeSwati <swati1993kushwaha@gmail.com>
2022-11-06 23:24:50 +0530

    changes

--- index.js
+++ index.js
@@ -16,18 +16,6 @@ const PORT = process.env.PORT || 5000
 
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
-
-
-
-
-
 app.use("/todo",todoRouter)
 
 app.get("/",(req,res)=>{
